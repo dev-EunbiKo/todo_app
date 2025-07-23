8b058d1e6c2a837c1509c67b263bd4d39e246d1f
dev_ebko <dev.ebko.ved@gmail.com>
2025-07-23 11:46:03 +0900

    drawer 추가, 링크 이동을 위한 url_launcher 패키지 설치

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/add_task.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -13,21 +14,43 @@ class _MainScreenState extends State<MainScreen> {
   // 리스트 방식으로 화면에 보여주기 위한 리스트 변수
   List<String> todoList = [];
 
-  // 데이터 전달 방법 1
+  // 추가 기능
   void addTodo({required String todoText}) {
+    // 중복 방지
+    if (todoList.contains(todoText)) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Already Exist!"),
+            content: Text("This task is already Exists!"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Close"),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
     setState(() {
       // 정렬을 위해 add -> insert 로 변경
       todoList.insert(0, todoText);
     });
 
-    writeLocalData();
+    saveTodo();
 
     // bottom sheet 닫기
     Navigator.pop(context);
   }
 
   // 저장 기능
-  void writeLocalData() async {
+  void saveTodo() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setStringList('todoList', todoList);
   }
@@ -49,7 +72,40 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(child: Text("Drawer")),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: const Text("kombi"),
+              accountEmail: const Text("12128_17@naver.com"),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(child: Image.asset("images/codingchef.png")),
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                launchUrl(Uri.parse("https://www.youtube.com"));
+              },
+              leading: const Icon(Icons.youtube_searched_for_rounded),
+              title: const Text("About Me"),
+            ),
+            ListTile(
+              onTap: () {
+                launchUrl(Uri.parse("https://www.naver.com"));
+              },
+              leading: const Icon(Icons.mail_outline_rounded),
+              title: const Text("Email"),
+            ),
+            ListTile(
+              onTap: () {
+                launchUrl(Uri.parse("https://www.naver.com"));
+              },
+              leading: const Icon(Icons.share_rounded),
+              title: const Text("share"),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: const Text('TODO App'),
         centerTitle: true,
@@ -89,7 +145,7 @@ class _MainScreenState extends State<MainScreen> {
                         setState(() {
                           todoList.removeAt(index);
                         });
-                        writeLocalData();
+                        saveTodo();
                         Navigator.pop(context);
                       },
                       child: const Text("Task Done!"),
